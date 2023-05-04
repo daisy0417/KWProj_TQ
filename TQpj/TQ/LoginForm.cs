0acb6c03db0862364c0d022be7141eb1445895e7
Youngbae1126 <rcplaza2663@gmail.com>
2023-05-05 01:06:10 +0900

    로그인 폼에서 에러 메시지 팝업 창 띄움

--- TQpj/TQ/LoginForm.cs
+++ TQpj/TQ/LoginForm.cs
@@ -25,10 +25,27 @@ namespace TQ
 
         private void loginBtn_Click(object sender, EventArgs e)
         {
-            this.Hide();
-            MainForm main = new MainForm();
-            main.ShowDialog();
-            this.Close();
+            if(nameTxtBox.Text=="" || ServerTxtBox.Text=="")
+            {
+                MessageBox.Show("이름과 IP를 정확히 입력해주세요.", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                this.Hide();
+                MainForm main = new MainForm();
+                main.ShowDialog();
+                this.Close();
+            }
+            
+        }
+
+        private void ServerTxtBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 숫자와 백스페이스만 입력 가능
+            if(!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back) || e.KeyChar == 46))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
--- TQpj/TQ/Program.cs
+++ TQpj/TQ/Program.cs
@@ -12,7 +12,7 @@ namespace TQ
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
