ee907e306c1c63e5bbcfafba592d43d512600514
Youngbae1126 <rcplaza2663@gmail.com>
2023-05-04 21:24:34 +0900

    Create Login Form

---
+++ TQpj/TQ/LoginForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TQ
+{
+    public partial class LoginForm : MetroFramework.Forms.MetroForm
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        public void txtBox()
+        {
+            // Create an instance of the TextBox control
+            nameTxtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MainForm main = new MainForm();
+            main.ShowDialog();
+            this.Close();
+        }
+    }
+}
