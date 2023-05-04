0b1415586661d376fe229b33451431ab0ae49b62
Youngbae1126 <rcplaza2663@gmail.com>
2023-05-05 01:15:02 +0900

    second Login Form commit

--- TQpj/TQ/Program.cs
+++ TQpj/TQ/Program.cs
@@ -12,7 +12,7 @@ namespace TQ
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
         [STAThread]
-        public static void Main()
+        static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
