0348364f7f50f932baafa8f4b4f98b1a1436ff48
이정원 <won31080@gmail.com>
2023-05-06 19:20:34 +0900

    Clinet, Server 추가 및 수정.

--- TQpj/TQ/client.cs
+++ TQpj/TQ/client.cs
@@ -10,66 +10,136 @@ using System.IO;
 
 namespace TQ
 {
-    internal class client
+    public partial class Client
     {
-        const int portmain = 5000;
-        IPAddress ip = IPAddress.Parse("127.0.0.1");
         StreamReader sreader;
         StreamWriter swriter;
-        int room;
-        void client_start() //도입부,스레드로 connect 호출
+
+        ClientForm parentForm;
+
+        public string username = "None";
+        private bool activate = false;
+        public bool Activate { get { return activate; } } //현재 클라이언트가 서버와 접속중인지
+
+        public Client(ClientForm parentForm)
         {
-            Thread thread = new Thread(connect_room);
-            thread.IsBackground = true;
-            thread.Start();
+            this.parentForm = parentForm;
         }
-        void send(string msg)
+
+        //서버와 접속을 시작한다.
+        public void Start()
         {
-            swriter.WriteLine(msg);
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect(IPAddress.Parse("127.0.0.1"), 5000);
+                swriter = new StreamWriter(client.GetStream());
+                swriter.WriteLine(0);
+                swriter.Close();
+                client.Close();
+
+                TcpClient client2 = new TcpClient();
+                client2.Connect(IPAddress.Parse("127.0.0.1"), 5000);
+                sreader = new StreamReader(client2.GetStream());
+                int port = int.Parse(sreader.ReadLine());
+                sreader.Close();
+                client.Close();
+
+                client = new TcpClient();
+                client.Connect(IPAddress.Parse("127.0.0.1"), port);
+                sreader = new StreamReader(client.GetStream());
+                swriter = new StreamWriter(client.GetStream());
+                swriter.AutoFlush = true;
+
+                Thread thread = new Thread(Connecting);
+                thread.IsBackground = true;
+                thread.Start();
+
+                activate = true;
+            }
+            catch
+            {
+                parentForm.ShowMessageBox("서버 연결 실패");
+            }
         }
-        void connect_room()// 입력받은 방 번호를 서버에 전송하고 포트 받아옴
-                           //이후 서버의 메세지 수신하는 역할
+
+        //서버에 요청을 보낸다.
+        private void SendRequest(string header, string content)
         {
-            TcpClient client = new TcpClient();
-            client.Connect(ip, portmain);
-            swriter = new StreamWriter(client.GetStream());
-            swriter.WriteLine(room);
-            swriter.Close();
-            client.Close();
+            swriter.WriteLine(header + "|" + content);
+        }
 
-            TcpClient client2 = new TcpClient();
-            client2.Connect(ip, portmain);
-            sreader = new StreamReader(client2.GetStream());
-            int port = int.Parse(sreader.ReadLine());
-            sreader.Close();
-            client.Close();
-
-            client = new TcpClient();
-            client.Connect(ip, port);
-            sreader = new StreamReader(client.GetStream());
-            swriter = new StreamWriter(client.GetStream());
-            swriter.AutoFlush = true;
-
+        //응답을 대기하는 부분. 스레드로 실행됨
+        private void Connecting()
+        {
             while (true)
             {
                 string msg = sreader.ReadLine();
-                Console.WriteLine(msg);
+                ResponseProcess(msg);
             }
+        }
 
-        }
-        /*
-        static void Main(string[] args)
+        //Server - chat_server에서 보낸 응답을 처리
+        private void ResponseProcess(string msg)
         {
-            client a = new client();
-            Console.WriteLine("room?:");
-            a.room = int.Parse(Console.ReadLine());
-            a.client_start();
-            while (true)
+            string[] response = msg.Split('|');
+            string header = response[0];
+            string content = response[1];
+
+            if (header.Equals("NONE"))
             {
-                string msg = Console.ReadLine();
-                a.send(msg);
+                parentForm.ShowMessageBox(content);
+            }
+            else if (header.Equals("GETROOM"))
+            {
+                parentForm.ViewRoom(int.Parse(content));
+            }
+            else if (header.Equals("SIGNIN"))
+            {
+                parentForm.SignIn(content);
+            }
+            else if (header.Equals("SIGNUP"))
+            {
+                parentForm.SignUp(content.Equals("1"));
+            }
+            else if (header.Equals("ROOMLIST"))
+            {
+                if (!content.Equals(string.Empty))
+                {
+                    List<string> roomList = content.Split(':').ToList();
+                    parentForm.RoomList(roomList);
+                }
+                else
+                {
+                    parentForm.RoomList(new List<string>());
+                }
+
+            }
+            else if (header.Equals("ROOMCREATE"))
+            {
+                parentForm.RoomCreate(content.Equals("1"));
+            }
+            else if (header.Equals("ROOMJOIN"))
+            {
+                parentForm.RoomJoin(content);
+            }
+            else if (header.Equals("ROOMOUT"))
+            {
+                parentForm.RoomOut();
+            }
+            else if (header.Equals("ROOMCHAT"))
+            {
+                if (!content.Equals(string.Empty))
+                {
+                    List<string> roomChatList = content.Split(',').ToList();
+                    parentForm.RoomChat(roomChatList);
+                }
+                else
+                {
+                    parentForm.RoomChat(new List<string>());
+                }
             }
         }
-        */
+
     }
 }
